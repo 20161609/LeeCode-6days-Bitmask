13ab4a5218fabe6e809df7c17c36403e6c70547e
Hojun An <pockguy96@gmail.com>
2025-11-12 14:52:54 -0500

    feat: add problem 78's solution.

--- solutions/solution_78.py
+++ solutions/solution_78.py
@@ -1,1 +1,6 @@
 ﻿# https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        return [[nums[i] for i in range(n) if m&(1<<i)] for m in range(1<<n)]

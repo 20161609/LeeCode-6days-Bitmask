666f0dbaaa7a08de72b295e10f729d3361819b92
Hojun An <pockguy96@gmail.com>
2025-11-13 15:07:46 -0500

    feat: add problem 137 solution.

--- solutions/solution_137.py
+++ solutions/solution_137.py
@@ -1,2 +1,9 @@
-﻿# https://leetcode.com/problems/single-number-ii/
+﻿# https://leetcode.com/problems/single-number-ii/?utm_source=chatgpt.com
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for b in range(32):
+            remainder = sum((num+2**31)>>b & 1 for num in nums) % 3
+            result += remainder*2**b
+        return result - 2**31

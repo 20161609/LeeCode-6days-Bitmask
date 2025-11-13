977043f190094cdd76c6393d1abdb5aeca8e9688
Hojun An <pockguy96@gmail.com>
2025-11-13 15:21:58 -0500

    feat: add problem 190's solution.

--- solutions/solution_190.py
+++ solutions/solution_190.py
@@ -1,2 +1,10 @@
 # https://leetcode.com/problems/reverse-bits/description/?utm_source=chatgpt.com
 
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        answer = 0
+        for i in range(31):
+            answer += (n%2)
+            answer *= 2
+            n//=2
+        return answer
--- solutions/solution_90.py
+++ solutions/solution_90.py
@@ -1,1 +1,2 @@
 ﻿# https://leetcode.com/problems/subsets-ii/
+

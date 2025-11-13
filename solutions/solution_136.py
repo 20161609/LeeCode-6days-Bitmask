a9e8f89468d7852cc6d56bc32c753564a97c1fe2
Hojun An <pockguy96@gmail.com>
2025-11-13 13:22:05 -0500

    feat: add problem 136's solution.

--- solutions/solution_136.py
+++ solutions/solution_136.py
@@ -1,2 +1,6 @@
 # https://leetcode.com/problems/single-number/description/?utm_source=chatgpt.com
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = sum(2**b*(sum((v+(1<<32))>>b&1 for v in nums)%2) for b in range(32))
+        return a if a<=(1<<31) else a-(1<<32)

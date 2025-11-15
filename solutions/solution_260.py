3a88d2f058194af1688b8c01ed801bcebee35c5d
Hojun An <pockguy96@gmail.com>
2025-11-14 20:13:31 -0500

    feat: add problem 260's solution.

--- solutions/solution_260.py
+++ solutions/solution_260.py
@@ -2,13 +2,11 @@
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        result = 0
-        for b in range(32):
-            cnt = 0
-            for num in nums:
-                num += 2**31
-                cnt += (num>>b) & 1
-            result += (cnt%2)*(2**b)
-        print(result)
-        print(result-(2**31))
-        
+        xor_sum = 0
+        for num in nums:
+            xor_sum ^= num
+        a, b = 0, 0
+        for num in nums:
+            if num & -xor_sum & xor_sum: a ^= num
+            else: b ^= num
+        return [a,b]
